0e93dbcace09d2520d9929ae3547197d787a112a
Addidaan <adriana48_rs@hotmail.com>
2023-02-13 11:59:13 -0600

    intalacion react, SPA rutas

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Wall from './components/Wall';
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/login' element={<Login />}></Route>
+      <Route path='/register' element={<Register />}></Route>
+      <Route path='/wall' element={<Wall />}></Route>
+  </Routes>
+)
+}
+
+export default App;
