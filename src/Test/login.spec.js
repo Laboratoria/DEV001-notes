8c6539df86d299b84403fb0746ad635826536737
GabyGonher <114185794+GabyGonher@users.noreply.github.com>
2023-03-20 23:07:24 -0600

    configuracion para test

---
+++ src/Test/login.spec.js
@@ -0,0 +1,13 @@
+import loginWhitGoogle from "../Firebase/func";
+// import {LogIn } from "../LogIn";
+
+it('loginWhitGoogle', () => {
+  // inicializacion
+  // estimulo
+  // observamos el comportamiento 
+  loginWhitGoogle()
+})
+
+describe('login', () => {
+  it('si ejecutamos la funcion login no debe fallar').toBe()
+})
